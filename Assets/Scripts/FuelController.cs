94b42e84edc07481e1fb035a77e5a950b7b843f5
Rustam Ismagilov <rustam.ismagilove@gmail.com>
2024-09-27 17:58:04 +0200

    Added "Add fuel" action
    When player will collide with game object with "Fuel" tag assigned, then add fuel set in fuelToAdd variable

--- Assets/Scripts/FuelController.cs
+++ Assets/Scripts/FuelController.cs
@@ -6,24 +6,48 @@ public class FuelController : MonoBehaviour
 {
     PlayerController player;
 
-    [SerializeField] public float currentFuel = 10f;
+    [HideInInspector] public float currentFuel;
 
     [SerializeField] float burnRate = 0.05f;
     [SerializeField] float maxFuel = 10f;
+    [SerializeField] float fuelToAdd = 20f; // Amount of fuel to add when colliding with a fuel pickup
 
     private void Start()
     {
         player = FindObjectOfType<PlayerController>();
+        currentFuel = maxFuel; // Initialize current fuel to the maximum at the start
     }
+
     public void ConsumeFuel()
     {
         currentFuel -= burnRate * Time.deltaTime;
+        currentFuel = Mathf.Clamp(currentFuel, 0f, maxFuel); // Clamp to ensure it doesn't go below 0
+        Debug.Log(currentFuel);
     }
 
     public void OutOfFuel()
     {
         player.enabled = false;
         Debug.Log("Out of fuel");
+        // add all other things related to crash sequence here
+
+
+
         //Time.timeScale = 0;
+    }
+
+    public void AddFuel(float amount)
+    {
+        currentFuel += amount;
+        currentFuel = Mathf.Clamp(currentFuel, 0f, maxFuel); // Clamp to ensure it doesn't exceed maxFuel
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Fuel"))
+        {
+            AddFuel(fuelToAdd);
+            Destroy(other.gameObject);
+        }
     }
 }
